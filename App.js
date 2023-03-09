641af922c8d939372fe14e64a3b7265813907abb
david <isebarn182@gmail.com>
2023-03-09 20:36:32 +0000

    Added overview

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text, Button, ImageBackground } from 'react-native';
+import { StyleSheet, Image, View, Text, BackHandler, ImageBackground } from 'react-native';
 import axios from 'axios';
 import RenderHtml, { defaultSystemFonts } from 'react-native-render-html';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -8,7 +8,6 @@ import { useFonts } from 'expo-font';
 
 const Layout1 = ({ story, chapterId, shaddowHeight, onPress }) => {
   let url = "https://isebarn-vid.s3.eu-west-2.amazonaws.com/"
-
   // find the chapter with the id of chapterId
   let chapter = story.chapters.find((chapter) => chapter.id === chapterId);
 
@@ -72,17 +71,81 @@ const Layout1 = ({ story, chapterId, shaddowHeight, onPress }) => {
   );
 }
 
+const HomeScreen = ({data, onPress}) => {
+  let url = "https://isebarn-vid.s3.eu-west-2.amazonaws.com/"
+
+  // return a view iterates the stories in data and for each story it makes a ImageBackground with a LinearGradient and a Text with the story.name and if clicked, it calls the Layout1 method with the story.chapters[0].id
+  return (
+    <View style={styles.container}>
+      {data.map((story) => (
+        <ImageBackground key={story.id} source={{ uri: url + story.id + "/original" }} style={{
+          flex: 1,
+          resizeMode: 'cover',
+          justifyContent: 'center'
+        }}
+        >
+          <LinearGradient colors={['transparent', ...Array(2).fill('black')]}
+            style={{ flex: 1 }}
+            start={{ x: 0, y: 0.5 }}
+            end={{ x: 0, y: 1 }}>
+            <View style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: 'rgba(0,0,0,.2)'
+            }}></View> 
+            <Text style={{ fontSize: 30, color: "#BDFDFD", justifyContent: 'center' }} onPress={() => {
+              console.log(story.id)
+              onPress(story.id)
+            }}>{story.name}</Text>
+          </LinearGradient>
+        </ImageBackground>
+      ))}
+    </View>
+  );
+}
+
+
+
+
 export default function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [chapterId, setChapterId] = useState('')
+  const [chapterId, setChapterId] = useState(null)
+  const [storyId, setStoryId] = useState(null)
+
+  useEffect(() => {
+    const backAction = () => {
+      if (chapterId) {
+        setChapterId(null)
+      } else if (storyId) {
+        setStoryId(null)
+      } else {
+        BackHandler.exitApp()
+      }
+      return true;
+    };
+  
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    ); 
+    
+    return () => backHandler.remove();
+  }, [chapterId, storyId]);
+
+  
 
 
-  const handlePress = (newChapterId) => {
+  const handlePressChapter = (newChapterId) => {
     setChapterId(newChapterId);
   };
 
-
+  const handlePressStory = (storyId) => {
+    console.log(storyId)
+    setStoryId(storyId);
+  };
+    
   const [loaded] = useFonts({
     MedievalSharp: require('./assets/fonts/medievalsharp/MedievalSharp.ttf'),
     Orbitron: require('./assets/fonts/Orbitron-VariableFont_wght.ttf'),
@@ -105,13 +168,27 @@ export default function App() {
 
   if (loading) {
     return <Text>Loading...</Text>;
-  }
-  return <Layout1
-    story={data[0]}
-    chapterId={chapterId}
+  } else if (!storyId) {
+    return <HomeScreen
+      data={data}
+      onPress={handlePressStory}
+    />
+  } else if (!chapterId) {
+    return <Layout1
+    story={data.find(story => story.id === storyId)}
+    chapterId={data.find(story => story.id === storyId).chapters[0].id}
     shaddowHeight={2}
-    onPress={handlePress}
-  />
+    onPress={handlePressChapter}  
+    />      
+  } else {
+    return <Layout1
+      story={data.find(story => story.id === storyId)}
+      chapterId={chapterId}
+      shaddowHeight={2}
+      onPress={handlePressChapter}  
+    />
+  }
+
 
 }
 
